7f4ad9d94d34f3d0c37ec76623e326c280166576
Sabina Markova <binamarkova@gmail.com>
2023-11-29 17:50:23 +0100

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,31 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurant = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    @restaurant.save
+    redirect_to restaurants_path(@restaurant)
+  end
+
+  def top
+    @restaurants = Restaurant.where(rating: 5)
+  end
+
+   private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
